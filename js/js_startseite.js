68c7f580dbbdcfc43a101e7513d7c04c6de91622
tobiaszehnder21 <49069555+tobiaszehnder21@users.noreply.github.com>
2019-05-14 21:22:45 +0200

    push1
    push1

---
+++ js/js_startseite.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+var video = document.getElementById("background");
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+	
+var currentScrollpos = window.pageYOffset;
+if(prevScrollpos > currentScrollpos) {
+	document.getElementById("header").style.top = "0";
+} else {
+	document.getElementById("header").style.top = "-100px";
+}
+	
+prevScrollpos = currentScrollpos;
+	
+}
+
+
