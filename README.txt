24ef667bbc9b83344c0bc15fc71c380ab2ee4869
tobiaszehnder21 <49069555+tobiaszehnder21@users.noreply.github.com>
2019-05-24 00:01:19 +0200

    PHP added

---
+++ README.txt
@@ -0,0 +1,5 @@
+Guten Tag Herr Garavaldi.
+
+Bitte installieren sie die beiden Fonts, welche ich Ihnen im Fonts Order bereitgestellt habe. So können Sie meine Seite in voller pracht geniessen!
+
+LG Tobias Zehnder
---
+++ php/run.php
@@ -0,0 +1,31 @@
+<?php
+$name = $_GET['name'];
+$email = $_GET['email'];
+$product = $_GET['product'];
+$text = $_GET['text'];
+
+echo $name;
+echo $email;
+echo $product;
+echo $text;
+
+$empfanger = "toodledesigns.order@gmail.com";
+$betreff = "Auftrag erhalten!";
+$nachricht = "$name hat was bestellt! \nEr will: $product. \nEs soll so aussehen: $text. \nSeine E-mail lautet: $email";
+$header = "From: $name <$email>";
+	
+mail($empfanger, $betreff, $nachricht, $header);	
+?>
+
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Order recieved</title>
+</head>
+
+<body>
+	<h1>Your order has been recieved!</h1>
+</body>
+</html>
